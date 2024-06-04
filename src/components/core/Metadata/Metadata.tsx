36e844052b917053ffb0076ba7f1f29f28846946
Renan Alonso <renan.alonso91@gmail.com>
2024-06-04 15:21:47 -0300

    feat: metadata component

---
+++ src/components/core/Metadata/Metadata.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+import { IMetadata } from './MetadataTypes';
+
+const Metadata = ({ title, description, metatags }: IMetadata) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keywords' content={metatags} />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+  );
+};
+export default Metadata;

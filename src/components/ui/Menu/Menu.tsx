e67bca9f1f852ce1e12fd398e60819dafc2608b7
Renan Alonso <renan.alonso91@gmail.com>
2024-06-15 23:50:57 -0300

    feat: menu initial structure

---
+++ src/components/ui/Menu/Menu.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link'
+
+const Menu = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/#portfolio">Portfolio</Link>
+          </li>
+          <li>
+            <Link href="/#about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+export default Menu

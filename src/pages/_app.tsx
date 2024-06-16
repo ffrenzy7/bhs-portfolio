f50d3790a61d45715bcba53bee14ec3cba173385
Renan Alonso <renan.alonso91@gmail.com>
2024-06-15 23:50:57 -0300

    feat: menu initial structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { Inter } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+import Menu from '@/components/ui/Menu'
+
 import 'wipe.css'
 import '@/styles/styles.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Menu />
+      <Component {...pageProps} />
+    </>
+  )
 }

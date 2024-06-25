8d1f6a4871fd37ba1fd1b14ba01ddaac8f23bfd5
Renan Alonso <renan.alonso91@gmail.com>
2024-06-25 19:34:34 -0300

    feat: portfolio section animations

--- src/components/ui/ProjectCard/ProjectCard.tsx
+++ src/components/ui/ProjectCard/ProjectCard.tsx
@@ -1,4 +1,5 @@
 // import { useState } from 'react'
+import { useRef } from 'react'
 import Link from 'next/link'
 import clsx from 'clsx'
 
@@ -6,9 +7,36 @@ import { IProjectCard } from './ProjectCardTypes'
 
 import s from './ProjectCard.module.scss'
 
+const THRESHOLD = 15
+
 const ProjectCard = ({ title, tags, image, url }: IProjectCard) => {
+  const cardRef = useRef<HTMLAnchorElement>(null)
+
+  const handleHover = (event: MouseEvent) => {
+    const { clientX, clientY, currentTarget } = event
+    const { clientWidth, clientHeight, offsetLeft, offsetTop } =
+      currentTarget as HTMLAnchorElement
+
+    const horizontal = (clientX - offsetLeft) / clientWidth
+    const vertical = (clientY - offsetTop) / clientHeight
+    const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2)
+    const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2)
+
+    cardRef.current.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`
+  }
+
+  const resetStyles = (event: MouseEvent) => {
+    cardRef.current.style.transform = `perspective(${event.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`
+  }
+
   return (
-    <Link className={clsx(s.card)} href={url}>
+    <Link
+      className={clsx(s.card)}
+      href={url}
+      onMouseMove={handleHover}
+      onMouseLeave={resetStyles}
+      ref={cardRef}
+    >
       <div className={clsx(s.imageWrapper)}>
         <img className={clsx(s.image)} src={image} />
       </div>
@@ -27,7 +55,17 @@ const ProjectCard = ({ title, tags, image, url }: IProjectCard) => {
         </div>
       </div>
 
-      <button className={clsx(s.button)}>Explorar</button>
+      <div className={clsx(s.buttonWrapper)}>
+        <div className={clsx(s.line)}></div>
+        <button className={clsx(s.button)}>
+          <div className={clsx(s.border, s.leftBottom)}></div>
+          <div className={clsx(s.border, s.bottom)}></div>
+          <div className={clsx(s.border, s.right)}></div>
+          <div className={clsx(s.border, s.top)}></div>
+          <div className={clsx(s.border, s.leftTop)}></div>
+          Explorar
+        </button>
+      </div>
     </Link>
   )
 }

4c890539061f33b341d0b5ff3ce738cd5bc80ff4
Renan Alonso <renan.alonso91@gmail.com>
2024-06-25 22:39:26 -0300

    feat: card animation tests

--- src/components/ui/ProjectCard/ProjectCard.tsx
+++ src/components/ui/ProjectCard/ProjectCard.tsx
@@ -7,34 +7,67 @@ import { IProjectCard } from './ProjectCardTypes'
 
 import s from './ProjectCard.module.scss'
 
-const THRESHOLD = 15
+// const THRESHOLD = 15
 
 const ProjectCard = ({ title, tags, image, url }: IProjectCard) => {
   const cardRef = useRef<HTMLAnchorElement>(null)
 
-  const handleHover = (event: MouseEvent) => {
-    const { clientX, clientY, currentTarget } = event
-    const { clientWidth, clientHeight, offsetLeft, offsetTop } =
-      currentTarget as HTMLAnchorElement
+  //TENTATIVA 1:
 
-    const horizontal = (clientX - offsetLeft) / clientWidth
-    const vertical = (clientY - offsetTop) / clientHeight
-    const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2)
-    const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2)
+  // const handleHover = (event: MouseEvent) => {
+  //   const { clientX, clientY, currentTarget } = event
+  //   const { clientWidth, clientHeight, offsetLeft, offsetTop } =
+  //     currentTarget as HTMLAnchorElement
 
-    cardRef.current.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`
-  }
+  //   const horizontal = (clientX - offsetLeft) / clientWidth
+  //   const vertical = (clientY - offsetTop) / clientHeight
+  //   const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2)
+  //   const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2)
 
-  const resetStyles = (event: MouseEvent) => {
-    cardRef.current.style.transform = `perspective(${event.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`
-  }
+  //   cardRef.current.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`
+  // }
+
+  // const resetStyles = (event: MouseEvent) => {
+  //   cardRef.current.style.transform = `perspective(${event.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`
+  // }
+
+  //TENTATIVA 2:
+
+  // let bounds
+
+  // const rotateToMouse = (e) => {
+  //   const mouseX = e.clientX
+  //   const mouseY = e.clientY
+  //   const leftX = mouseX - bounds.x
+  //   const topY = mouseY - bounds.y
+  //   const center = {
+  //     x: leftX - bounds.width / 2,
+  //     y: topY - bounds.height / 2,
+  //   }
+  //   const distance = Math.sqrt(center.x ** 2 + center.y ** 2)
+
+  //   cardRef.style.transform = `
+  //     rotate3d(
+  //       ${center.y / 100},
+  //       ${-center.x / 100},
+  //       0,
+  //       ${Math.log(distance) * 2}deg
+  //     )
+  //   `
+  // }
 
   return (
     <Link
       className={clsx(s.card)}
       href={url}
-      onMouseMove={handleHover}
-      onMouseLeave={resetStyles}
+      // onMouseEnter={() => {
+      //   bounds = cardRef.getBoundingClientRect()
+      //   document.addEventListener('mousemove', rotateToMouse)
+      // }}
+      // onMouseLeave={() => {
+      //   document.removeEventListener('mousemove', rotateToMouse)
+      //   cardRef.style.transform = ''
+      // }}
       ref={cardRef}
     >
       <div className={clsx(s.imageWrapper)}>

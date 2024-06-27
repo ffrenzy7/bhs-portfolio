f6bf841ed7be0a642b90dd9a8c13438075d3ff4d
Renan Alonso <renan.alonso91@gmail.com>
2024-06-27 18:50:57 -0300

    feat: portfolio cards animation

--- src/components/ui/ProjectCard/ProjectCard.tsx
+++ src/components/ui/ProjectCard/ProjectCard.tsx
@@ -1,5 +1,4 @@
-// import { useState } from 'react'
-import { useRef } from 'react'
+import { useRef, MouseEvent } from 'react'
 import Link from 'next/link'
 import clsx from 'clsx'
 
@@ -7,99 +6,89 @@ import { IProjectCard } from './ProjectCardTypes'
 
 import s from './ProjectCard.module.scss'
 
-// const THRESHOLD = 15
-
 const ProjectCard = ({ title, tags, image, url }: IProjectCard) => {
   const cardRef = useRef<HTMLAnchorElement>(null)
+  const cardRect = useRef<DOMRect>()
 
-  //TENTATIVA 1:
+  const onMouseEnter = () => {
+    if (!cardRef.current) return
 
-  // const handleHover = (event: MouseEvent) => {
-  //   const { clientX, clientY, currentTarget } = event
-  //   const { clientWidth, clientHeight, offsetLeft, offsetTop } =
-  //     currentTarget as HTMLAnchorElement
+    cardRect.current = cardRef.current.getBoundingClientRect()
+  }
 
-  //   const horizontal = (clientX - offsetLeft) / clientWidth
-  //   const vertical = (clientY - offsetTop) / clientHeight
-  //   const rotateX = (THRESHOLD / 2 - horizontal * THRESHOLD).toFixed(2)
-  //   const rotateY = (vertical * THRESHOLD - THRESHOLD / 2).toFixed(2)
+  const onMouseMove = (event: MouseEvent<HTMLAnchorElement>) => {
+    if (!cardRect.current || !cardRef.current) return
 
-  //   cardRef.current.style.transform = `perspective(${clientWidth}px) rotateX(${rotateY}deg) rotateY(${rotateX}deg) scale3d(1, 1, 1)`
-  // }
+    const mouseX = event.clientX
+    const mouseY = event.clientY
+    const leftX = mouseX - cardRect.current.x
+    const topY = mouseY - cardRect.current.y
 
-  // const resetStyles = (event: MouseEvent) => {
-  //   cardRef.current.style.transform = `perspective(${event.currentTarget.clientWidth}px) rotateX(0deg) rotateY(0deg)`
-  // }
+    const center = {
+      x: leftX - cardRect.current.width / 2,
+      y: topY - cardRect.current.height / 2,
+    }
+    const distance = Math.sqrt(center.x ** 2 + center.y ** 2)
 
-  //TENTATIVA 2:
+    const intensity = 3
 
-  // let bounds
+    cardRef.current.style.transform = `
+      rotate3d(
+        ${center.y / 100},
+        ${-center.x / 100},
+        0,
+        ${Math.log(distance) * intensity}deg
+      )
+    `
+  }
 
-  // const rotateToMouse = (e) => {
-  //   const mouseX = e.clientX
-  //   const mouseY = e.clientY
-  //   const leftX = mouseX - bounds.x
-  //   const topY = mouseY - bounds.y
-  //   const center = {
-  //     x: leftX - bounds.width / 2,
-  //     y: topY - bounds.height / 2,
-  //   }
-  //   const distance = Math.sqrt(center.x ** 2 + center.y ** 2)
+  const onMouseLeave = () => {
+    if (!cardRef.current) return
 
-  //   cardRef.style.transform = `
-  //     rotate3d(
-  //       ${center.y / 100},
-  //       ${-center.x / 100},
-  //       0,
-  //       ${Math.log(distance) * 2}deg
-  //     )
-  //   `
-  // }
+    cardRef.current.style.transform = ''
+  }
 
   return (
-    <Link
-      className={clsx(s.card)}
-      href={url}
-      // onMouseEnter={() => {
-      //   bounds = cardRef.getBoundingClientRect()
-      //   document.addEventListener('mousemove', rotateToMouse)
-      // }}
-      // onMouseLeave={() => {
-      //   document.removeEventListener('mousemove', rotateToMouse)
-      //   cardRef.style.transform = ''
-      // }}
-      ref={cardRef}
-    >
-      <div className={clsx(s.imageWrapper)}>
-        <img className={clsx(s.image)} src={image} />
-      </div>
-
-      <div className={clsx(s.content)}>
-        <h3 className={clsx(s.title)}>{title}</h3>
-
-        <div className={clsx(s.tags)}>
-          <span className={clsx(s.dash)}>- </span>
-          {tags.map((tag, index) => (
-            <span key={index} className={clsx(s.tag)}>
-              {tag}
-              {index < tags.length - 1 ? ', ' : ''}
-            </span>
-          ))}
+    <div className={clsx(s.cardWrapper)}>
+      <Link
+        className={clsx(s.card)}
+        href={url}
+        onMouseEnter={onMouseEnter}
+        onMouseMove={onMouseMove}
+        onMouseLeave={onMouseLeave}
+        ref={cardRef}
+      >
+        <div className={clsx(s.imageWrapper)}>
+          <img className={clsx(s.image)} src={image} />
         </div>
-      </div>
 
-      <div className={clsx(s.buttonWrapper)}>
-        <div className={clsx(s.line)}></div>
-        <button className={clsx(s.button)}>
-          <div className={clsx(s.border, s.leftBottom)}></div>
-          <div className={clsx(s.border, s.bottom)}></div>
-          <div className={clsx(s.border, s.right)}></div>
-          <div className={clsx(s.border, s.top)}></div>
-          <div className={clsx(s.border, s.leftTop)}></div>
-          Explorar
-        </button>
-      </div>
-    </Link>
+        <div className={clsx(s.content)}>
+          <h3 className={clsx(s.title)}>{title}</h3>
+
+          <div className={clsx(s.tags)}>
+            <span className={clsx(s.dash)}>- </span>
+            {tags.map((tag, index) => (
+              <span key={index} className={clsx(s.tag)}>
+                {tag}
+                {index < tags.length - 1 ? ', ' : ''}
+              </span>
+            ))}
+          </div>
+        </div>
+
+        <div className={clsx(s.buttonWrapper)}>
+          <div className={clsx(s.line)}></div>
+          <button className={clsx(s.button)}>
+            <div className={clsx(s.border, s.leftBottom)}></div>
+            <div className={clsx(s.border, s.bottom)}></div>
+            <div className={clsx(s.border, s.right)}></div>
+            <div className={clsx(s.border, s.top)}></div>
+            <div className={clsx(s.border, s.leftTop)}></div>
+            Explorar
+          </button>
+        </div>
+      </Link>
+    </div>
   )
 }
 

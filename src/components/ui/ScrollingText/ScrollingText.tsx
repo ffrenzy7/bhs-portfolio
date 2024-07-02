4a965b4714d4b747c19d9aed31b6952f5728865f
Renan Alonso <renan.alonso91@gmail.com>
2024-07-01 23:08:03 -0300

    feat: scrolling text component

---
+++ src/components/ui/ScrollingText/ScrollingText.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx'
+import { useEffect, useRef } from 'react'
+
+import { IScrollingText } from './ScrollingTextTypes'
+
+import s from './ScrollingText.module.scss'
+
+const ScrollingText = ({ children, className }: IScrollingText) => {
+  const elementRef = useRef<HTMLHeadingElement>(null)
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (!elementRef.current) return
+
+      const { top, bottom } = elementRef.current?.getBoundingClientRect() as DOMRect
+      const isElementVisible = top < window.innerHeight && bottom > 0
+
+      if (isElementVisible) {
+        elementRef.current.style.transform = `translateX(${-window.scrollY * 0.02}%)`
+      }
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
+  return (
+    <div className={clsx(s.wrapper, className)} ref={elementRef}>
+      {children}
+    </div>
+  )
+}
+
+export default ScrollingText

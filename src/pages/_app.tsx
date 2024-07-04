dffbf062bba61fa3b153dcf92dc58f7662d1e7e4
Renan Alonso <renan.alonso91@gmail.com>
2024-07-04 02:50:34 -0300

    feat: back to top component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Poppins } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
 import Menu from '@/components/ui/Menu'
+import BackToTop from '@/components/ui/BackToTop'
 
 import 'wipe.css'
 import '@/styles/styles.scss'
@@ -16,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={poppins.className}>
       <Menu />
+      <BackToTop />
       <Component {...pageProps} />
     </div>
   )
